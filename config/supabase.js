7acaecf0fecf9da75839307ced73adbe8f0a5acc
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2026-02-25 13:58:30 +0530

    Supabase config

--- config/supabase.js
+++ config/supabase.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_KEY exists:", !!process.env.SUPABASE_KEY);
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
